0d634f6707dfe08cc13903998a0450e70b6ce133
Alexander Koryakin <sanek31rus@gmail.com>
2019-12-05 20:24:33 +0300

    Добавил проект сгенерированный в Spring Initalizr

---
+++ src/main/java/ru/bstu/ieitus/handmade_backend/HandmadeBackendApplication.java
@@ -0,0 +1,13 @@
+package ru.bstu.ieitus.handmade_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HandmadeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HandmadeBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/bstu/ieitus/handmade_backend/HandmadeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.bstu.ieitus.handmade_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HandmadeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
